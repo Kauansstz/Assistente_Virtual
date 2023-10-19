6dd0119ec1fcd459d4b948df7a27489bff27fd39
Kauan <kauansantosdesouza45@gmail.com>
2023-10-19 18:33:08 -0300

    Alterado o nome do script  bot para bot_pyautogui e criado um novo script com chatterbot

---
+++ bot_chatterbot.py
@@ -0,0 +1,11 @@
+# Corrigir bugs
+# from spacy.cli import dowload
+
+# dowload("en_core_websm")
+
+# class ENGSM:
+#   ISO_639_1 = 'en_core_web_sm'
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+ListTrainer 
