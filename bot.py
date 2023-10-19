3ffe57490413755915e345f048ca51239fc3ff97
Kauan <kauansantosdesouza45@gmail.com>
2023-10-19 15:58:46 -0300

    Aprendendo como criar um bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.PAUSE = 2
+
+pyautogui.press("win")
+pyautogui.write("opera")
+pyautogui.press("enter")
+pyautogui.click(x=500, y=50)
+pyautogui.write("https://www.youtube.com/")
+pyautogui.press("enter")
+pyautogui.click(x=800, y=130)
+pyautogui.write("vinicius13")
+pyautogui.press("enter")
