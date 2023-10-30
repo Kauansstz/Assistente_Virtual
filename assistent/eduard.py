71563166ed42d30aa464e998da9babf422fe8757
Kauan <kauansantosdesouza45@gmail.com>
2023-10-29 21:39:05 -0300

    Adicionado a função de calculadora e inserido funções ao invés de deixar as respostas flutuando

--- assistent/eduard.py
+++ assistent/eduard.py
@@ -1,28 +1,30 @@
-from respostas import CALENDARIO, CLIMA
+from respostas import calendary, climate_of_the_region, calculator
 
 while True:
     saucao_bot = print("Seja Bem-Vindo!\nSou Eduard! Seu assistente virtual.")
-    saucao_user = input("Como posso te ajudar?\nR: ")
+    greetings_user = input("Como posso te ajudar?\nR: ")
+    keyword_search = "pesquisa"
+    keyword_calculator = "calcular"
+    first_keyword = ["pesquisa", "calcular"]
+    if greetings_user in first_keyword:
+        if greetings_user == "pesquisa":
+            response = input("Digite o que deseja Pesquisar\nR: ")
+            second_keyword = ["calendario", "clima"]
 
-    if saucao_user == "pesquisa":
-        print(
-            "Digite um palavra que seja precisa\nExemplo: 'clima', irá retorna como está o clima da sua região"
-        )
+            if response in second_keyword:
+                if response == "calendario":
+                    for i in calendary():
+                        if response in second_keyword:
+                            print(i)
 
-        lista = ["tempo", "clima", "Horas", "dia", "calendario"]
+                elif response == "clima":
+                    print(climate_of_the_region())
+            else:
+                print("Desculpe! Não consegui identificar o que disse")
 
-        print("Digite o que deseja Pesquisar")
-        print(f"Aqui estão uma lista de opções para pesquisar.", lista)
-        response = input("O que seja pesquisar?\nR: ")
+        elif greetings_user == "calcular":
+            calculator()
 
-        for i in CALENDARIO:
-            perguntas = i
-            if response == "calendario" or saucao_user == "Calendario":
-                print(i)
-
-        if response == "clima" or saucao_user == "Clima":
-            print(CLIMA)
-
-        continua = input("Posso te ajudar algo a mais?\nR: ")
-        if continua.lower() == "Nao" or continua.lower() == "nao":
-            break
+    continua = input("Posso te ajudar algo a mais?\nR: ")
+    if continua.lower() == "Nao" or continua.lower() == "nao":
+        break
--- assistent/respostas.py
+++ assistent/respostas.py
@@ -1,17 +1,53 @@
 import datetime
 import requests
 
-HORA = datetime.datetime.now().strftime(
-    "Horas: %H:%M"
-)  # Coletando as horas via console(Desktop/Mobile)
 
-DIA = datetime.datetime.now().strftime(
-    "Data: %D - Dia da Semana: %A"
-)  # Coletando o dia, mes, ano e dia da semana via console(Desktop/Mobile)
+def calendary():
+    HORA = datetime.datetime.now().strftime(
+        "Horas: %H:%M"
+    )  # Coletando as horas via console(Desktop/Mobile)
+    DIA = datetime.datetime.now().strftime(
+        "Data: %D - Dia da Semana: %A"
+    )  # Coletando o dia, mes, ano e dia da semana via console(Desktop/Mobile)
+    return DIA, HORA  # Juntando tudo para mostrar ao usuário as informações coletadas
 
-CALENDARIO = DIA, HORA  # Juntando tudo para mostrar ao usuário as informações coletadas
 
-api = "https://wttr.in/palhoça"  # Pegando uma api com clima, porcentagem de chuva nos 4 horarios(Manhã, começo/Fim da tarde e de Noite)
+def climate_of_the_region():
+    api = "https://wttr.in/palhoça"  # Pegando uma api com clima, porcentagem de chuva nos 4 horarios(Manhã, começo/Fim da tarde e de Noite)
+    response = requests.get(api)  # Variavel recebendo o requests da api
+    CLIMA = response.text  # Convertendo a variavel acima em forma de texto
+    return CLIMA
 
-response = requests.get(api)  # Variavel recebendo o requests da api
-CLIMA = response.text  # Convertendo a variavel acima em forma de texto
+
+def calculator():
+    operator = input("Digite qualquer sinal de operação\nR: ")
+
+    number_1 = input("Digite um número\nR: ")
+    number_2 = input("Digite um número\nR: ")
+
+    number_1_float = float(number_1)
+    number_2_float = float(number_2)
+
+    def more(x, y):
+        return print(x + y)
+
+    def any_less(x, y):
+        return print(x - y)
+
+    def multiply(x, y):
+        return print(x * y)
+
+    def to_divide(x, y):
+        return print(x / y)
+
+    if operator == "+":
+        return more(number_1_float, number_2_float)
+
+    if operator == "-":
+        return any_less(number_1_float, number_2_float)
+
+    if operator == "*":
+        return multiply(number_1_float, number_2_float)
+
+    if operator == "/":
+        return to_divide(number_1_float, number_2_float)
