c90292b0de87e78bfdfd1d6f4651dceb14360cdc
Kauan <kauansantosdesouza45@gmail.com>
2024-05-17 10:55:22 -0300

    problema no dicionario. Consegue identificar somente 3 funções que são: clima, calendario, esta e sair. As demais, não consegue identificar

--- project_aurora/aurora/aurora.py
+++ project_aurora/aurora/aurora.py
@@ -90,16 +90,7 @@ while True:
             os.system("cls")
             print("Eu estou bem!")
         if greetings_user.lower() in base["dic"]["about"]:
-            sleep(1)
-            print(
-                "Em 2022, nasceu Aurora, uma assistente pessoal revolucionária criada por Kauan dos Santos de Souza aos seus jovens 19 anos.\
-                  Sua jornada para criar Aurora começou quando ele estava trabalhando como helpdesk, lidando com uma variedade de consultas e problemas dos usuários. \
-                  Foi durante esse tempo que Kauan percebeu uma necessidade crucial: um assistente que pudesse compreender profundamente os seres humanos e suas necessidades."
-                "Motivado pela visão de facilitar a vida das pessoas, Kauan embarcou em uma missão para desenvolver um assistente que fosse mais do que apenas uma ferramenta \
-                  técnica. Ele queria criar uma presença virtual que fosse empática, compreensiva e capaz de proporcionar suporte de maneira eficaz e humana."
-                "Assim nasceu Aurora. Inspirada pela luz do amanhecer, ela representa um novo começo, oferecendo um raio de esperança e assistência em meio às complexidades da vida cotidiana. \
-                  Equipada com habilidades avançadas de compreensão de linguagem natural e uma compreensão profunda das necessidades humanas, Aurora está pronta para oferecer suporte, orientação e companhia a todos que precisam dela."
-            )
+            rp.about()
 
         if greetings_user.lower() in base["dic"]["sair"]:
             sleep(1)
--- project_aurora/aurora/response.py
+++ project_aurora/aurora/response.py
@@ -164,3 +164,16 @@ def cote(type_money):
         money_btn_float = float(money_btn)
         money_round = round(money_btn_float, 2)
         return print("Cotação do Bitcoin está $", money_round)
+
+
+def about():
+    sleep(1)
+    print(
+        "Em 2022, nasceu Aurora, uma assistente pessoal revolucionária criada por Kauan dos Santos de Souza aos seus jovens 19 anos.\
+            Sua jornada para criar Aurora começou quando ele estava trabalhando como helpdesk, lidando com uma variedade de consultas e problemas dos usuários. \
+            Foi durante esse tempo que Kauan percebeu uma necessidade crucial: um assistente que pudesse compreender profundamente os seres humanos e suas necessidades.\n"
+        "Motivado pela visão de facilitar a vida das pessoas, Kauan embarcou em uma missão para desenvolver um assistente que fosse mais do que apenas uma ferramenta \
+            técnica. Ele queria criar uma presença virtual que fosse empática, compreensiva e capaz de proporcionar suporte de maneira eficaz e humana.\n"
+        "Assim nasceu Aurora. Inspirada pela luz do amanhecer, ela representa um novo começo, oferecendo um raio de esperança e assistência em meio às complexidades da vida cotidiana. \
+            Equipada com habilidades avançadas de compreensão de linguagem natural e uma compreensão profunda das necessidades humanas, Aurora está pronta para oferecer suporte, orientação e companhia a todos que precisam dela."
+    )
